752751253645053b455ac0b37117d7689dd4695a
Gang Cheng <gangc@uw.edu>
2016-06-10 16:33:04 -0700

    alter confidence interval

--- src/bart_new.jl
+++ src/bart_new.jl
@@ -678,7 +678,9 @@ function StatsBase.predict(bart::Bart,x::Matrix{Float64},confidence_interval::Bo
       for i in range(1,length(bart.bart_additive_trees))
       y_predict[:,i] = predict(bart.bart_additive_trees[i],x)
       y_predict[:,i] = denormalize(y_predict[:,i],bart.y_min,bart.y_max)
-      y_CI[i,:] = quantile(vec(y_predict[:,i]),[0.025,0.975])
+      end
+      for i in 1:size(x,2)
+        y_CI[i,:] = quantile(vec(y_predict[i,:]),[0.025,0.975])
       end
       #point estimate
       y_hat = vec(mean(y_predict,2))
